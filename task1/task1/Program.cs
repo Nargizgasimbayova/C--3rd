31579c0c5c36b3611781cb269793422db8d542c9
Nargiz Gasimbayova <you@example.com>
2022-08-08 15:45:41 +0400

    First task

---
+++ task1/task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Console.ReadLine(); int count=0;
+
+            Console.WriteLine(count);
+        }
+
+        static int NumLenght(int n)
+        {
+            int count = 0;
+            while (n != 0)
+            {
+                n = n / 10;
+                ++count;
+            }
+            return count;
+
+        }
+           
+
+}
+
+
+}
