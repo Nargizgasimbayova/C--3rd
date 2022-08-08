8c2d3eb96b8ad6e298cb69fca7ad42c9017a8214
Nargiz Gasimbayova <you@example.com>
2022-08-08 15:46:52 +0400

    second task

---
+++ task2/task2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            FindVowel(out string text);
+            Console.WriteLine(count);
+        }
+
+        string stirng FindVowel(out string text)
+        {
+            int count = 0;
+            switch (text)
+            {
+                case ('a'):
+                    count++;
+                    break;
+                case ('e'):
+                    count++;
+                    break;
+                case ('i'):
+                    count++;
+                    break;
+                case ('o'):
+                    count++;
+                    break;
+                case ('u'):
+                    count++;
+                    break;
+                default:
+                    Console.WriteLine("There is no vowel letter");
+                    break;
+
+            }
+
+            return count;
+        }       
+    }
+}
